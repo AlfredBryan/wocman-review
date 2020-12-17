de8742b4fe4b42f18373d659b90ab4538f827553
KingAbesh <abeshekwere@gmail.com>
2020-12-17 15:22:36 +0100

    feat: doing some magic

--- src/containers/wocman/pages/dashboard.jsx
+++ src/containers/wocman/pages/dashboard.jsx
@@ -25,80 +25,85 @@ export const Dashboard = () => {
     },
   ];
 
-  return (
-    <Flex
-      justify="center"
-      w="100%"
-      h="fit-content"
-      flex={1}
-      flexDir={{ base: "column", xl: "row" }}
-    >
+	return (
+    <Box>
       <Flex
-        flex={{ base: 1, xl: 1.8 }}
-        flexDirection="column"
-        p={{ base: 4, md: 8 }}
+        justify="center"
+        w="100%"
+        flex={1}
+        flexDir={{ base: "column", xl: "row" }}
       >
-        <Wallet />
         <Flex
-          justify={{ base: "center", md: "space-between" }}
-          align="center"
-          w="100%"
-          my={10}
-          flexDir={{ base: "column", md: "row" }}
+          flex={{ base: 1, xl: 1.8 }}
+          flexDirection="column"
+          p={{ base: 4, md: 8 }}
         >
-          <Card text="Work Done" number="18" sub="Jobs done" />
-          <Card text="Rating" number="5" sub="stars" />
-          <Card text="Completion %" number="70%" sub="completion" noMargin />
+          <Wallet />
+          <Flex
+            justify={{ base: "center", md: "space-between" }}
+            align="center"
+            w="100%"
+            my={10}
+            flexDir={{ base: "column", md: "row" }}
+          >
+            <Card text="Work Done" number="18" sub="Jobs done" />
+            <Card text="Rating" number="5" sub="stars" />
+            <Card text="Completion %" number="70%" sub="completion" noMargin />
+          </Flex>
+          <Notifications />
         </Flex>
-        <Notifications />
-      </Flex>
-      <Flex w="100%" px={{ base: 4, md: 8, xl: 0 }} flex={{ base: 1, xl: 1.2 }}>
         <Flex
-          flex={1}
-          backgroundColor="wocman.sideNav"
-          flexDir="column"
-          px={{ base: 4, md: 8 }}
-          py={{ base: 8, md: 16 }}
-          overflowY={{ base: "none", xl: "scroll" }}
-          position="static"
-          h={{ base: "auto", xl: "100vh" }}
+          w="100%"
+          px={{ base: 4, md: 8, xl: 0 }}
+          flex={{ base: 1, xl: 1.2 }}
         >
-          <Flex flexDir="column">
-            <Text
-              fontFamily="Poppins"
-              fontWeight="500"
-              fontSize={{ base: "1.2rem", md: "1.4rem", xl: "1.8rem" }}
-              lineHeight="20px"
-              mb={{ base: 4, md: 8 }}
-            >
-              Today's Schedule
-            </Text>
-            <Text
-              fontFamily="Poppins"
-              fontWeight="normal"
-              fontSize={{ base: "0.8rem", md: "1rem", xl: "1.2rem" }}
-              lineHeight="20px"
-            >
+          <Flex
+            flex={1}
+            backgroundColor="wocman.sideNav"
+            flexDir="column"
+            px={{ base: 4, md: 8 }}
+            py={{ base: 8, md: 16 }}
+            overflowY={{ base: "none", xl: "scroll" }}
+            position="static"
+            h={{ base: "auto", xl: "100vh" }}
+          >
+            <Flex flexDir="column">
               <Text
-                as="span"
                 fontFamily="Poppins"
-                fontWeight="600"
+                fontWeight="500"
+                fontSize={{ base: "1.2rem", md: "1.4rem", xl: "1.8rem" }}
+                lineHeight="20px"
+                mb={{ base: 4, md: 8 }}
+              >
+                Today's Schedule
+              </Text>
+              <Text
+                fontFamily="Poppins"
+                fontWeight="normal"
                 fontSize={{ base: "0.8rem", md: "1rem", xl: "1.2rem" }}
                 lineHeight="20px"
               >
-                Tuesday
-              </Text>{" "}
-              25 Dec
-            </Text>
-          </Flex>
-          <Flex w="100%" mt={8} flexDir="column">
-            {activities.map((item, index) => (
-              <CalendarSection key={index} item={item} index={index} />
-            ))}
+                <Text
+                  as="span"
+                  fontFamily="Poppins"
+                  fontWeight="600"
+                  fontSize={{ base: "0.8rem", md: "1rem", xl: "1.2rem" }}
+                  lineHeight="20px"
+                >
+                  Tuesday
+                </Text>{" "}
+                25 Dec
+              </Text>
+            </Flex>
+            <Flex w="100%" mt={8} flexDir="column">
+              {activities.map((item, index) => (
+                <CalendarSection key={index} item={item} index={index} />
+              ))}
+            </Flex>
           </Flex>
         </Flex>
       </Flex>
-    </Flex>
+    </Box>
   );
 };
 
