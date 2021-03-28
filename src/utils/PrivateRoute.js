a81c685be0ae3c874f3c5bbe10380fc783da7134
Erons <eronmmer@gmail.com>
2021-03-28 18:39:11 +0100

    chore: change authentication implementation

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -1,19 +1,13 @@
 import * as React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { useSelector } from "react-redux";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-	const { isLoading, isAuthenticated } = useSelector(
-		({ login: { isLoading, isAuthenticated } = {} }) => ({
-			isLoading,
-			isAuthenticated,
-		})
-	);
+	const isAuthenticated = !!localStorage["wocman_user"] &!!localStorage["wocman_token"]
 	return (
 		<Route
 			{...rest}
 			render={(props) =>
-				!isLoading && !isAuthenticated ? (
+				!isAuthenticated ? (
 					<Redirect to="/login" />
 				) : (
 					<Component {...props} />
